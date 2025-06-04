175981bc2c232362a1dfc792699a65a9835b4462
tranhoang03 <tranhoang0320@gmail.com>
2025-06-04 14:21:24 +0700

    Update face_auth.py

--- system/face_auth.py
+++ system/face_auth.py
@@ -151,7 +151,7 @@ class FaceAuthTransformer:
 
 
 
-def find_matching_face(embedding, threshold=0.2):
+def find_matching_face(embedding, threshold=0.21):
     conn = None
     best_match_info = None
     max_similarity = -1.0
